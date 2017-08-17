62270737b02748c23660190f392ed986e07c990d
Andy Alderton <andrewalderton@sky.com>
2017-08-17 17:56:44 +0100

    style: add code comments

--- logs_analysis.py
+++ logs_analysis.py
@@ -2,9 +2,11 @@
 
 import psycopg2
 
+# Connect to the PostgreSQL database
 conn = psycopg2.connect("dbname=news")
 cursor = conn.cursor()
 
+# What are the most popular three articles of all time?
 cursor.execute(
     """SELECT title, count(*) FROM articles
     LEFT JOIN log ON log.path
@@ -18,6 +20,7 @@ print("\nMost popular 3 articles of all time:\n")
 for i in range(0, len(results), 1):
     print("\'" + results[i][0] + "\' - " + str(results[i][1]) + " views")
 
+# Who are the most popular article authors of all time?
 cursor.execute(
     """SELECT name, count(*) FROM authors
     LEFT JOIN articles ON articles.author = authors.id
@@ -31,6 +34,7 @@ print("\n\nMost popular article authors of all time:\n")
 for i in range(0, len(results), 1):
     print(results[i][0] + " - " + str(results[i][1]) + " views")
 
+# On which days did more than 1% of requests lead to errors?
 cursor.execute(
     """SELECT DATE(time) as date, sum(case when status = '404 NOT FOUND' then 1
     else 0 end) / (count(log.time)/100)::float as error
@@ -45,4 +49,5 @@ for i in range(0, len(results), 1):
     print(str(results[i][0]) + " - " + str(round(results[i][1], 2)) +
           "% errors\n")
 
+# Close the database connection
 conn.close()
